a3d35dc438c4e377a4523e7fa9df456eaa866aa5
ishini <dimanij-se20032@stu.kln.ac.lk>
2024-06-14 13:47:35 +0530

    Auth Routes Setup Added

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup = (req, res) => {
+  res.send("Signup user");
+  console.log("Signup user");
+};
+
+export const login = (req, res) => {
+  console.log("Login user");
+};
+
+export const logout = (req, res) => {
+  console.log("Logout user");
+};
