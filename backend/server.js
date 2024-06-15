2cfaf0ddd8ea7bde054c3772b01c7abe3f5ff318
ishini <dimanij-se20032@stu.kln.ac.lk>
2024-06-15 23:54:49 +0530

    Get Users for Sidebar Endpoint Created

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMondoDB.js";
 
 const app = express();
@@ -16,6 +18,7 @@ app.use(cookieParser()); // to parse incoming requests with cookies
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // app.get("/", (req, res) => {
 //   // root route http://localhost:5000/
